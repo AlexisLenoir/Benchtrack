1596df0ede7f7ff40a07132edb14ce204ba08730
LeonaireHo <50570354+LeonaireHo@users.noreply.github.com>
2021-02-19 11:44:22 +0100

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,27 +1,14 @@
 import os
-import os
-import time
-import datetime
-#Pour trouver tous les fichers de repertoire de BASE.
-def find_all_file(base):
-    for root, ds, fs in os.walk(base):
-        for f in fs:
-            yield f
 
-#Pour calculer temps d'execution
-def exe_python(target_name):
-    start = datetime.datetime.now()
-    print("Test "+target_name+" start")
-    os.system("python " + target_name)
-    timeUsed = (datetime.datetime.now() - start).microseconds * 1e-6
-    print("Test " + target_name +" finished using time:" + timeUsed.__str__())
-    return timeUsed
+from benchTrack import *
+from resultsBench import ResultsBench
 
-# #execute test of bench
-# def exe_bench(bench_name):
-#     for i in find_all_file(bench_name):
-#         exe_python(i)
 #test
-base = 'PGM/bench1/'
-for i in find_all_file(base):
-    t = exe_python(base + i)
+List_Result_Benchs = []
+bench_name = 'bench1'
+res = exe_bench(bench_name)
+List_Result_Benchs.append(res)
+print(res.__str__())
+
+to_txt(List_Result_Benchs)
+
