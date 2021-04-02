b57d62c179ba1f925bc827b559283f715e29f556
xavieryangzhua <xavieryangzhuang@gmail.com>
2021-04-02 14:56:49 +0200

    this is a test of before

---
+++ ConfigFichier/tasks/default/charger_employe/before_xml_demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  2 14:53:33 2021
+
+@author: radar
+"""
+
+import numpy
+import sys
+print("This is a test of before_xml_demo.py")
