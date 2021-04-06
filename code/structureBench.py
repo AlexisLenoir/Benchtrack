5fbfd24333a30b881cdddc2f5174c8805f3f81a0
Leonaire <hexin0515@gmail.com>
2021-04-06 14:48:09 +0200

    separate structureBench.py in task.py,theme.py,structureBench.py

--- code/structureBench.py
+++ code/structureBench.py
@@ -1,109 +1,6 @@
-#class Task
-import os
-import re
 from tools import *
-class Task:
-    def __init__(self,name,args,sample_size=20):
-        self.__Name = name
-        self.__dictTargets = {}
-        self.__sample_size = sample_size
-        self.args = args
-
-    def __str__(self):
-        string = self.getName() + "["
-        for i in self.__dictTargets:
-            string += i + " "
-        return string + "]"
-
-    def addTarget(self, name_Target, res_Target = -1):
-        self.__dictTargets [name_Target] = res_Target
-
-    def modifyTarget(self, name_Target, res_Target):
-        self.__dictTargets [name_Target] = res_Target
-
-    def exe_task(self,lis,benchName,themeName,path):
-        print("-In Task "+self.__Name+":")
-        for i in self.__dictTargets:
-            if i in lis:
-                #time to execute before_test
-                times_before = time.time()
-                self.exe_before_test(benchName,themeName,i,path)
-                times_before = time.time() - times_before
-
-                #time to execute test
-                start = time.time()
-                self.exe_target(benchName,themeName,i,path)
-                self.__dictTargets[i] = (time.time() - start - times_before)/self.__sample_size
-                print("--execute target " + i +' '+self.__sample_size.__str__()+' times')
-                print("Execution time:"+self.__dictTargets[i].__str__())
-
-
-    def exe_before_test(self,benchName,themeName,targets,path):
-        path_File = path + benchName + "/tasks/" + themeName + "/" + self.getName() + "/" + "before_" + targets
-        path_ConfigFile = path + benchName + "/targets/" + targets
-        self.exe_file(path_ConfigFile, path_File)
-        
-    def exe_target(self,benchName,themeName,targets,path):
-        path_ConfigFile = path + benchName + "/targets/" + targets
-        path_File =path + benchName + "/tasks/" + themeName + "/" + self.getName() + targets
-        self.exe_file(path_ConfigFile,path_File)
-
-    def exe_file(self,path_configFile,args,path_file):
-        if os.path.exists(path_configFile):
-            command, language = ConfigFileTask(path_configFile)
-            for n in range(self.__sample_size):
-                if os.path.exists(path_file):
-                    exeCmd(path_file,args, command, language)
-        else:
-            print("Can't find the file config:"+path_file)
-
-    def getName(self):
-        return self.__Name
-    def ToCsv(self,writer,theme):
-        targetNames = []
-        targetNames = dict.keys(self.__dictTargets)
-        for target in targetNames:
-            run_time = self.__dictTargets[target]
-            writer.writerow([theme, self.__Name, target, run_time])
-
-#theme
-class Theme:
-    def __init__(self,name):
-        self.__themeName = name
-        self.__listTasks = []
-
-    def __str__(self):
-        string = self.getName()
-        string += "["+self.__listTasks[0].__str__()
-        for i in range(1,len(self.__listTasks)):
-            string += "," + self.__listTasks[i].__str__()
-        string += "]"
-        return string
-    def addTask(self,task):
-        self.__listTasks.append(task)
-    def getName(self):
-        return self.__themeName
-
-    def exe_theme(self,listTask,listTarget,benchName,path):
-        print("\nIn theme " + self.getName() + ":")
-        for i in range(len(self.__listTasks)):
-            # print(listTask)
-            if self.__listTasks[i].getName() in listTask:
-                self.__listTasks[i].exe_task(listTarget,benchName,self.__themeName,path)
-
-    def showlistTasks(self):
-        print("In the theme " + self.getName() + ",there are tasks:")
-        for i in self.__listTasks:
-            print(i.getName())
-    def showInfoTask(self,nameTask,nameTest,path):
-        for i in self.__listTasks:
-            if i.getName() == nameTask:
-                file_read(path+nameTest,nameTask,'tasks')
-                return 1
-    def ToCsv(self,writer):
-        for task in self.__listTasks:
-            task.ToCsv(writer,self.__themeName)
-#class BenchTrack,
+from theme import Theme
+#class BenchTrack
 class BenchTrack:
     def __init__(self,path_inf):
         self.__path = path_inf
@@ -169,24 +66,6 @@ class BenchTrack:
                         task.addTarget(targetName)
                         
                 theme.addTask(task)
-                #for every target in the folder task
-                # for i in os.listdir(pathTs):
-                #     if re.match("times",i):
-                #         with open(pathTs+i, newline='') as csvfile:
-                #             spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-                #             next(spamreader)
-                #             for row in spamreader:
-                #                 aux = re.split(',|\[|\]|\'',row.__str__())
-                #                 aux = [x for x in aux if x != '']
-                #                 task.addTargetTimes(aux[0],int(aux[1]))
-                #         continue
-                #     ret = re.match("README|data", i)
-                #     if not ret:
-                #         ret = re.match("[A-Z,a-z,_,-]*", i)
-                #         # print(ret.group())
-                #         pathF = pathTs + '/' + i
-                #         task.addTarget(ret.group(),-1)
-                #         task.addTarget(ret.group(),-1)
 
             self.__listThemes.append(theme)
             return True
---
+++ code/theme.py
@@ -0,0 +1,40 @@
+from tools import *
+from task import Task
+#theme
+
+class Theme:
+    def __init__(self,name):
+        self.__themeName = name
+        self.__listTasks = []
+
+    def __str__(self):
+        string = self.getName()
+        string += "["+self.__listTasks[0].__str__()
+        for i in range(1,len(self.__listTasks)):
+            string += "," + self.__listTasks[i].__str__()
+        string += "]"
+        return string
+    def addTask(self,task):
+        self.__listTasks.append(task)
+    def getName(self):
+        return self.__themeName
+
+    def exe_theme(self,listTask,listTarget,benchName,path):
+        print("\nIn theme " + self.getName() + ":")
+        for i in range(len(self.__listTasks)):
+            # print(listTask)
+            if self.__listTasks[i].getName() in listTask:
+                self.__listTasks[i].exe_task(listTarget,benchName,self.__themeName,path)
+
+    def showlistTasks(self):
+        print("In the theme " + self.getName() + ",there are tasks:")
+        for i in self.__listTasks:
+            print(i.getName())
+    def showInfoTask(self,nameTask,nameTest,path):
+        for i in self.__listTasks:
+            if i.getName() == nameTask:
+                file_read(path+nameTest,nameTask,'tasks')
+                return 1
+    def ToCsv(self,writer):
+        for task in self.__listTasks:
+            task.ToCsv(writer,self.__themeName)
