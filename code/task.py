e6773342cc9d48b517a3bf2b759d11dac9de882b
xavieryangzhua <xavieryangzhuang@gmail.com>
2021-04-14 15:56:32 +0200

    add some comments

--- code/task.py
+++ code/task.py
@@ -1,23 +1,94 @@
 from tools import *
 class Task:
     def __init__(self, name, args, sample_size=20):
+        '''
+        Constructor of class Task
+
+        Parameters
+        ----------
+        name : string
+            name of the task.
+        args : string
+            parameters of the task.
+        sample_size : int, optional
+            the times of execution. The default is 20.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__Name = name
         self.__dictTargets = {}
         self.__sample_size = int(sample_size)
         self.__args = args
     def __str__(self):
+        '''
+        Return a string of all the targets from the task
+
+        Returns
+        -------
+        stirng
+            all the targets from the task.
+
+        '''
         string = self.getName() + "["
         for i in self.__dictTargets:
             string += i + " "
         return string + "]"
 
     def addTarget(self, name_Target):
+        '''
+        Add targets to targets dictionary of the task
+
+        Parameters
+        ----------
+        name_Target : string
+            name of target.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__dictTargets[name_Target] = [-1] * len(self.__args)
 
     def modifyTarget(self, name_Target, res_Target):
+        '''
+        Add execution time of targets of the task
+
+        Parameters
+        ----------
+        name_Target : string
+            the target.
+        res_Target : flaot
+            execution time.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__dictTargets[name_Target] = res_Target
 
     def exe_task(self, lis, themeName, path):
+        '''
+        Execution of the tasks 
+
+        Parameters
+        ----------
+        lis : list of string
+            list of targets.
+        themeName : string
+            name of theme of the task.
+        path : stirng
+            the path contains the theme
+
+        Returns
+        -------
+        None.
+
+        '''
         print("-In Task " + self.__Name + ":")
         for i in self.__dictTargets:
             if i in lis:
@@ -38,16 +109,40 @@ class Task:
                 self.__dictTargets[i] = list_times
 
     def exe_before_test(self, themeName, targets, path,args):
+        '''
+        execute some before-task file like imports
+
+        Returns
+        -------
+        None.
+
+        '''
         path_File = path  + "/tasks/" + themeName + "/" + self.getName() + "/" + "before_" + targets
         path_ConfigFile = path  + "/targets/" + targets + "/config.ini"
         self.exe_file(path_ConfigFile, path_File,args)
 
     def exe_target(self, themeName, targets,path,args):
+        '''
+        drive infos from configure file of target and transforme these infos
+
+        Returns
+        -------
+        None.
+
+        '''
         path_ConfigFile = path  + "/targets/" + targets + "/config.ini"
         path_File = path  + "/tasks/" + themeName + "/" + self.getName() + "/" +targets
         self.exe_file(path_ConfigFile, path_File,args)
 
     def exe_file(self, path_configFile, path_file, args):
+        '''
+        execution of target
+
+        Returns
+        -------
+        None.
+
+        '''
         if os.path.exists(path_configFile):
             command, language = ConfigFileTarget(path_configFile)
             for n in range(self.__sample_size):
@@ -57,9 +152,27 @@ class Task:
             print("Can't find the file config:" + path_configFile)
 
     def getName(self):
+        '''
+        return name of the task
+
+        Returns
+        -------
+        string
+            name of the task.
+
+        '''
         return self.__Name
 
     def ToCsv(self, writer, theme):
+        '''
+        generetor the csv file which contains some
+        infos about results of execution
+
+        Returns
+        -------
+        None.
+
+        '''
         targetNames = []
         targetNames = dict.keys(self.__dictTargets)
         for target in targetNames:
@@ -68,6 +181,15 @@ class Task:
                 writer.writerow([theme, self.__Name, target,self.__args[i], run_time])
 
     def get_structure_tasks(self,path):
+        '''
+        get targets of tasks
+
+        Returns
+        -------
+        list_target : TYPE
+            DESCRIPTION.
+
+        '''
         list_target=[]
         for target in list(self.__dictTargets.keys()):
             path_configFile = path + "/targets/" + target + "/config.ini"
