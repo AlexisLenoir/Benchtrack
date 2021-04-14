0015c7683534194ddb9b80e390df53e8c4b7f490
xavieryangzhua <xavieryangzhuang@gmail.com>
2021-04-14 19:14:28 +0200

    add some comments

--- code/theme.py
+++ code/theme.py
@@ -3,10 +3,31 @@ from tools import *
 
 class Theme:
     def __init__(self,name):
+        '''
+        construtor of the class theme
+
+        Parameters
+        ----------
+        name : string
+            name of the theme.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__themeName = name
         self.__listTasks = []
 
     def __str__(self):
+        '''
+
+        Returns
+        -------
+        string : string
+            a string that contains all the tasks of the theme.
+
+        '''
         string = self.getName()
         string += "["+self.__listTasks[0].__str__()
         for i in range(1,len(self.__listTasks)):
@@ -14,11 +35,48 @@ class Theme:
         string += "]"
         return string
     def addTask(self,task):
+        '''
+        all task to the listTasks of the theme
+
+        Parameters
+        ----------
+        task : string
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__listTasks.append(task)
     def getName(self):
+        '''
+        getter of parameter Name of the theme
+
+        Returns
+        -------
+        TYPE string
+
+        '''
         return self.__themeName
 
     def exe_theme(self,listTask,listTarget,path):
+        '''
+        execution of the theme
+
+        Parameters
+        ----------
+        listTask : list of string
+            all the tasks of the theme.
+        listTarget : list of string
+            all the targets of the task in the theme.
+        path : string
+            the path contains the theme.
+
+        Returns
+        -------
+        None.
+
+        '''
         print("\nIn theme " + self.getName() + ":")
         for i in range(len(self.__listTasks)):
             # print(listTask)
@@ -26,19 +84,50 @@ class Theme:
                 self.__listTasks[i].exe_task(listTarget,self.__themeName,path)
 
     def showlistTasks(self):
+        '''
+        Output the list of tasks of the theme
+
+        Returns
+        -------
+        None.
+
+        '''
         print("In the theme " + self.getName() + ",there are tasks:")
         for i in self.__listTasks:
             print(i.getName())
     def showInfoTask(self,nameTask,nameTest,path):
+        '''
+        Output the Info(readme) file of the task in the theme
+
+        Parameters
+        ----------
+        nameTask : string
+        nameTest : string
+        path : string
+            the path contains the theme.
+
+        '''
         for i in self.__listTasks:
             if i.getName() == nameTask:
                 file_read(path+nameTest,nameTask,'tasks')
                 return 1
     def ToCsv(self,writer):
+        '''
+        To write list of tasks of the theme in csv file
+
+        Returns
+        -------
+        None.
+
+        '''
         for task in self.__listTasks:
             task.ToCsv(writer,self.__themeName)
 
     def get_structure_tasks(self,path):
+        '''
+        Get the structure of every task
+
+        '''
         list_structure_task = {}
         for task in self.__listTasks:
             list_structure_task[task.getName()] =task.get_structure_tasks(path)
