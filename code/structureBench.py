c215f2dcfa65bad79beccf18f64dbaac6c2f1030
xavieryangzhua <xavieryangzhuang@gmail.com>
2021-04-14 19:40:59 +0200

    add some comments

--- code/structureBench.py
+++ code/structureBench.py
@@ -3,7 +3,26 @@ from theme import Theme
 from task import Task
 #class BenchTrack
 class BenchTrack:
+    
     def __init__(self, path_inf, path_benchTrack):
+        print(path_inf)
+        print(path_benchTrack)
+        '''
+        constructor of class BenchTrack
+
+        Parameters
+        ----------
+        path_inf : string
+            the relative path that contains the infrastructure.
+            which is also the first parameter of input
+        path_benchTrack : string
+            the absolu path that contains the benchTrack.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__path = path_inf
         self.__name = path_inf.split('/')[-1]
         #dictionaire of targets,<key = nameTarget> = objetTarget
@@ -17,6 +36,15 @@ class BenchTrack:
             print("Error:Construct")
         self.__outputFile = self.__path+"/output.csv"
     def __str__(self):
+        '''
+        Output list of all the themes of the infrastructure
+
+        Returns
+        -------
+        string : TYPE
+            DESCRIPTION.
+
+        '''
         string = self.getName()
         string += ":list Themes["
         if len(self.__listThemes) != 0:
@@ -77,9 +105,26 @@ class BenchTrack:
             self.__listThemes.append(theme)
             return True
     def getName(self):
+        '''
+        Getter of parameter __name
+
+        Returns
+        -------
+        TYPE
+            DESCRIPTION.
+
+        '''
         return self.__name
 
     def ToCsv(self):
+        '''
+        write infos of the infrastructure to csv file
+
+        Returns
+        -------
+        None.
+
+        '''
         with open(self.__outputFile, "w",newline="") as csvfile:
             writer = csv.writer(csvfile)
             # name of columns
@@ -89,18 +134,71 @@ class BenchTrack:
                 theme.ToCsv(writer)
 
     def addTargets(self,name_target,lang_target):
+        '''
+        Add targets at the dictionay dictTargets
+
+        Parameters
+        ----------
+        name_target : string
+            targets of the infrastructure.
+        lang_target : string
+            programming languague of the target.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__dictTargets[name_target] = lang_target
 
     def __addThemes(self,theme):
+        '''
+        Add themes of the infrastructure to the listThemes
+
+        Parameters
+        ----------
+        theme : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        '''
         self.__listThemes.append(theme)
 
     # execution of bench
     def exe_bench(self):
+        '''
+        Execution of the programme
+
+        Returns
+        -------
+        None.
+
+        '''
         print("Execution de "+self.__name)
         for i in range(len(self.__listThemes)):
             self.__listThemes[i].exe_theme(self.__allTask,self.__allTarget,self.__path)
 
     def filter_target(self,lis,model):
+        '''
+        delet the target has been excluded from the command
+        of user
+
+        Parameters
+        ----------
+        lis : list of string
+            list of targets will be excluded/included.
+        model : Bool
+            if true, all the targets selected will be added in the list of execution.
+            else, delet those targets excluded
+
+        Returns
+        -------
+        None.
+
+        '''
         if model:
             self.__allTarget = lis
             # print(self.__allTarget)
@@ -110,6 +208,23 @@ class BenchTrack:
             # print(self.__allTarget)
 
     def filter_task(self,lis,model):
+        '''
+        delet the task has been excluded from the command
+        of user
+
+        Parameters
+        ----------
+        lis : list of string
+            list of tasks will be excluded/included.
+        model : Bool
+            if true, all the tasks selected will be added in the list of execution.
+            else, delet those tasks excluded
+
+        Returns
+        -------
+        None.
+
+        '''
         if model:
             self.__allTask = lis
             print(self.__allTask)
@@ -118,20 +233,59 @@ class BenchTrack:
             print(self.__allTask)
 
     def show_list_target(self):
+        '''
+        Output the list of targets that will be executed
+
+        Returns
+        -------
+        None.
+
+        '''
         print("List of targets:")
         for i in range(len(self.__allTarget)):
             print(self.__allTarget[i])
 
     def showInfoTarget(self,nomTarget):
+        '''
+        Output infos (readme) of the target 
+
+        Parameters
+        ----------
+        nomTarget : string
+            target's name.
+
+        Returns
+        -------
+        None.
+
+        '''
         print("Information of " + nomTarget + " in the test " + self.__name +":")
         file_read(self.__path+self.__name,nomTarget,"targets")
 
     def showListTasks(self):
+        '''
+        output all the tasks that will be executed
+
+        Returns
+        -------
+        None.
+
+        '''
         print("List of tasks:")
         for i in range(len(self.__listThemes)):
             self.__listThemes[i].showlistTasks()
 
     def showInfoTask(self,nameTask):
+        '''
+        output info(readme) of the task
+
+        Parameters
+        ----------
+        nameTask : string
+            task's name.
+
+
+        '''
         for i in self.__listThemes:
             if i.showInfoTask(nameTask,self.__name,self.__path):
                 return 1
@@ -139,6 +293,10 @@ class BenchTrack:
         return -1
 
     def get_structure_tasks(self):
+        '''
+        get the structure of the tasks
+
+        '''
         list_structure_theme = {}
         for theme in self.__listThemes:
             list_structure_theme[theme.getName()] = theme.get_structure_tasks(self.__path)
